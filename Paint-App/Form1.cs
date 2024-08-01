468e5edee9bd601369787fe9d1af8cdaf799bd1c
knuyg <uygunkn@gmail.com>
2024-08-01 23:48:16 +0200

    Add project files.

---
+++ Paint-App/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint_App
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing = false;
+        private Point lastPoint;
+        private Pen currentPen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            currentPen = new Pen(Color.Black, 5);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void drawingPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = true;
+                lastPoint = e.Location;
+            }
+        }
+
+        private void drawingPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = drawingPanel.CreateGraphics())
+                {
+                    g.DrawLine(currentPen, lastPoint, e.Location);
+                }
+                lastPoint = e.Location;
+            }
+        }
+
+        private void drawingPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                isDrawing = false;
+            }
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
